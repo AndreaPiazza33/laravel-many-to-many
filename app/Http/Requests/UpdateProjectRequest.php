5d6e933c0facd0601d1c8a2e38cacc0c0a6d93fa
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-10-30 02:00:53 +0100

    first commit

---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'=> [
+                'required',
+                 'string',
+                 Rule::unique('projects')->ignore($this->project->id)],
+            'description'=> ['required', 'string'],
+            'type_id'=> ['nullable', 'exists:types,id'],
+            'link'=> ['required', 'url']
+        ];
+    }
+
+    public function messages() {
+        return [
+            'title.required'=> 'Il titolo è obbligatorio',
+            'title.string'=> 'Il titolo deve essere una stringa',
+            'title.unique'=> 'Il titolo del progetto è già esistente',
+
+            'description.required'=> 'La descrizione è obbligatoria',
+            'description.string'=> 'La descrizione deve essere una stringa',
+
+            'link.required'=> 'Il link è obbligatorio',
+            'link.string'=> 'Il link deve essere un URL',
+
+            'type_id.exists' => 'La tipologia inserita non è valida',
+        ];
+    }
+}
